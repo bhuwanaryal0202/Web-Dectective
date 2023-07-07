206e030737020909b2acaaa668c01764f33cd169
bhuwanaryal <bhuwanaryal0202@gmail.com>
2023-07-07 16:11:09 +1000

    3rd commit

--- popup copy.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var pickButton = document.getElementById('pickButton');
-    var colorResult = document.getElementById('colorResult');
-    var fontsResult = document.getElementById('fontsResult');
-    var spacingResult = document.getElementById('spacingResult');
-  
-    
-  });
-  
